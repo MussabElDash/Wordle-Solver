9caa4774dac633d3eb1dad0c2d0f6c5f29a16f37
Mussab ElDash <mussab14@gmail.com>
2026-01-10 19:38:21 -0500

    Initial Commit

---
+++ Wordle Solver/Wordle_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_SolverApp.swift
+//  Wordle Solver
+//
+//  Created by Mussab ElDash on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
